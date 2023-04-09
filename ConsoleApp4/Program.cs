f114722ca4c6cb349a5e5bdc114a12c728c28cac
pawankamdi <pawankamdi26@gmail.com>
2023-04-09 15:49:45 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please entre the First Name");
+            String Firstname = Console.ReadLine();
+            Console.WriteLine("Please entre the Last Name");
+            String Lastname = Console.ReadLine();
+            Console.WriteLine("Full Name : {0} {1}", Firstname, Lastname);
+            Console.ReadLine();
+        }
+    }
+}
