7784d920587c8a4064b6d7d55c731bfb9be6b05d
pawankamdi <pawankamdi26@gmail.com>
2023-04-09 17:35:33 +0530

    Changes

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,12 +10,29 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please entre the First Name");
-            String Firstname = Console.ReadLine();
-            Console.WriteLine("Please entre the Last Name");
-            String Lastname = Console.ReadLine();
-            Console.WriteLine("Full Name : {0} {1}", Firstname, Lastname);
+            //    Console.WriteLine("Please entre the First Name");
+            //    String Firstname = Console.ReadLine();
+            //    Console.WriteLine("Please entre the Last Name");
+            //    String Lastname = Console.ReadLine();
+            //    Console.WriteLine("Full Name : {0} {1}", Firstname, Lastname);
+            //    Console.ReadLine();
+            Console.WriteLine("Please entre the number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == 10)
+            {
+                Console.WriteLine("TEN");
+            }
+            else if(input ==20)
+            {
+                Console.WriteLine("Twenty");
+            }
+            else
+            {
+                Console.WriteLine("Not Equql");
+            }
             Console.ReadLine();
+
         }
+       
     }
 }
